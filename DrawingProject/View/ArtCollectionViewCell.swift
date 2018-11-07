49897577990c76d0f01855c055d95abb7088aea6
Angela Rivas <snowcats001@gmail.com>
2018-11-07 13:36:10 -0700

    Added artOutlets to ArtCollectionControllerCell

---
+++ DrawingProject/View/ArtCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArtCollectionViewCell.swift
+//  DrawingProject
+//
+//  Created by Rivas, Angela on 11/7/18.
+//  Copyright © 2018 CTEC CSP. All rights reserved.
+//
+
+import UIKit
+
+class ArtCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var artLabel: UILabel!
+    @IBOutlet weak var artImage: UIImageView!
+}
